290edfe522271b473042cff4c5df1f4ce13843ec
Noam Lubofsky <noamlubofsky@gmail.com>
2021-07-21 16:36:57 -0400

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+# def initialize(name, birthday, hair_color, eye_color, height,
+#   weight, handed, complexion, t_shirt_size,
+#   wrist_size, glove_size, pant_length, pant_width)
+#   @name = name
+#   @birthday = birthday
+#   @hair_color = hair_color
+#   @eye_color = eye_color
+#   @weight = weight
+#   @handed = handed
+#   @complexion = complexion
+#   @t_shirt_size = t_shirt_size
+#   @wrist_size = wrist_size
+#   @glove_size = glove_size
+#   @pant_length = pant_length
+#   @pant_width = pant_width
+# end
